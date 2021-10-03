e5d4b47d6b4c8546a3065ce647147b07fedfd5a4
Angel Hernandez <Angelh289@gmail.com>
2021-10-03 09:53:29 -0700

    All tests passing for user story 4

--- back-end/src/tables/tables.service.js
+++ back-end/src/tables/tables.service.js
@@ -11,8 +11,23 @@ function list(){
 function read(table_id){
     return knex("tables").select("tables.*").where({"table_id":table_id}).first();
 }
+
+function findRes(reservation_id){
+    return knex("reservations").select("reservations.*").where({"reservation_id": reservation_id}).first();
+}
+
+function update(table_id, reservation_id){
+    return knex("tables")
+        .select("*")
+        .where({"table_id": table_id})
+        .update({ reservation_id: reservation_id, occupied: true })
+        
+    };
+
 module.exports = {
     list,
     create,
     read,
+    findRes,
+    update,
 }
