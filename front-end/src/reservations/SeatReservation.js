533529118ed16c624916233ce918bea2001b2e26
Angel Hernandez <Angelh289@gmail.com>
2021-10-11 14:27:55 -0700

    Seat reservation implementation.

--- front-end/src/reservations/SeatReservation.js
+++ front-end/src/reservations/SeatReservation.js
@@ -1,13 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {Link} from "react-router-dom";
+import { listTables } from "../utils/api";
 // seatReservation from "../utils/api"
 
 export default function SeatReservation(){
-    const [] = useState({});
+    const [tables, setTables] = useState([]);
+    const [tablesError, setTablesError] = useState(null);
 
+    useEffect(() => {
+        async function loadTables(){
+            const abortController = new AbortController();
+            setTables(null);
+            listTables(abortController.signal)
+                .then(setTables)
+                .catch(setTablesError)
+            return () => abortController.abort();
+        }
+    }, [])
+
+    const tableOptions = tables.map((table) => {
+        return  (<option value={table.table_name}>{table.table_name} - {table.capacity}</option>)
+    })
     return (
     <>
-    SeatReservation Working
+        <select name="table_id" >
+            {tableOptions}
+        </select>
     </>
         )
 }
