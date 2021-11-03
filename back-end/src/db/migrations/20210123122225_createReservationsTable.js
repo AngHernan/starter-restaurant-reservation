63e745d082b31c398260648dc47dd16a5249f700
Anngel Hernandez <angelh2896@gmail.com>
2021-11-03 10:28:35 -0700

    migration file

--- back-end/src/db/migrations/20210123122225_createReservationsTable.js
+++ back-end/src/db/migrations/20210123122225_createReservationsTable.js
@@ -1,6 +1,8 @@
 exports.up = function (knex) {
   return knex.schema.createTable("reservations", (table) => {
-    table.increments("reservation_id").primary();
+    table
+      .increments("reservation_id")
+      .primary();
     table.string("first_name")
     table.string("last_name")
     table.varchar("mobile_number")
@@ -8,11 +10,9 @@ exports.up = function (knex) {
     table.time("reservation_time")
     table.integer("people")
     table.string("status").defaultTo("booked");
-    table.timestamps(true, true);
-    
+    table.timestamps(true, true); 
   });
 };
-
 exports.down = function (knex) {
   return knex.schema.dropTable("reservations");
 };
--- back-end/src/db/migrations/20211006115847_createTablesTable.js
+++ back-end/src/db/migrations/20211006115847_createTablesTable.js
@@ -14,7 +14,6 @@ exports.up = function(knex) {
       table.timestamps(true, true);
   })
 };
-
 exports.down = function(knex) {
   return knex.schema.dropTable("tables");
 };
